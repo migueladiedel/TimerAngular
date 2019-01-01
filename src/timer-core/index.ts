cacc6a240be22179fc13f794c8ea0cb6837796ef
migueladiedel <maddgm92@gmail.com>
2019-01-01 02:33:38 +0100

    Borrado de modulo sin usar

--- src/timer-core/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './timer-core.module';
-export { LoggerService, ERROR_LEVEL } from './services/logger.service';
-export * from './pipes/cadenas.pipe';
-export * from './pipes/numericos.pipe';
-export { naturalNumberValidator, typeMismatchValidator, NIFValidator } from './directives/validaciones.directive';
--- src/timer-core/pipes/numericos.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToComaDecimalPipe } from './numericos.pipe';
-
-describe('ToComaDecimalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToComaDecimalPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
